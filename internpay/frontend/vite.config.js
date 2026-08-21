0ca82852ec9af9255e650d6bc9404252e3f86b8b
hetbharodiya <bharodiyahet@gmail.com>
2026-08-21 23:12:08 +0530

    vercel change

--- internpay/frontend/vite.config.js
+++ internpay/frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/static/' : '/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
